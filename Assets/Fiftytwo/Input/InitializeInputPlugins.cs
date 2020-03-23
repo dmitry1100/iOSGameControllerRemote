c37180871a48e3ca0584f011d78c006adfac31d7
Dmitry Victorov <dmitry.viktorov@gmail.com>
2020-03-23 18:10:21 +0300

    Initial commit

---
+++ Assets/Fiftytwo/Input/InitializeInputPlugins.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.iOS;
+using UnityEngine.InputSystem.Layouts;
+
+
+namespace Fiftytwo
+{
+    public static class InitializeInputPlugins
+    {
+#if UNITY_EDITOR
+        [UnityEditor.InitializeOnLoadMethod]
+#else
+        [RuntimeInitializeOnLoadMethod( RuntimeInitializeLoadType.BeforeSceneLoad )]
+#endif
+        private static void Initialize ()
+        {
+#if UNITY_EDITOR || UNITY_IOS || UNITY_TVOS
+            InputSystem.RegisterLayout<iOSGameControllerRemote>(
+                matches: new InputDeviceMatcher()
+                    .WithInterface( "iOS", false )
+                    .WithDeviceClass( "iOSGameController", false )
+                    .WithProduct( "Remote", false )
+            );
+#endif
+        }
+    }
+}
